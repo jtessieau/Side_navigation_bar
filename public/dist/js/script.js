ac20a93337a98dee118567499844b5ed07a1b0a3
Julien TESSIEAU <julien.tessieau@gmail.com>
2021-07-17 13:14:57 +0100

    add js script to toggle side menu on mouseover

--- public/dist/js/script.js
+++ public/dist/js/script.js
@@ -1,21 +1,19 @@
 const sideMenu = document.querySelector(".side-navigation");
-const toggleDisplay = document.querySelectorAll(".toggle-visibility");
+const toggleDisplay = document.querySelectorAll(".toggle");
 
 sideMenu.addEventListener("mouseenter", (e) => {
     if (sideMenu.style.width != "260px") {
-        sideMenu.style.width = "260px";
         setTimeout(() => {
             toggleDisplay.forEach((element) => {
-                element.style.display = "block";
+                element.classList.remove("toggle-visibility");
             });
         }, 200);
     }
 });
 sideMenu.addEventListener("mouseleave", (e) => {
     if (document.documentElement.clientWidth <= 780) {
-        sideMenu.style.width = "70px";
         toggleDisplay.forEach((element) => {
-            element.style.display = "none";
+            element.classList.add("toggle-visibility");
         });
     }
 });
